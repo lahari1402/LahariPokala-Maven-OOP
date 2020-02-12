cc4dff3fa21e210aaac9e932662fabe0cde0964c
lahari1402 <59884581+lahari1402@users.noreply.github.com>
2020-02-12 21:11:15 +0530

    final

---
+++ Candy.java
@@ -0,0 +1,14 @@
+package com.lahari.Project2;
+
+import com.rishitha.Project2.sweet;
+
+public class Candy extends sweet{
+      Candy(String name, int weight, int cost) {
+		super(name, weight, cost);
+		// TODO Auto-generated constructor stub
+	}
+
+	protected String getType(){
+    	  return "candy";
+      }
+}
